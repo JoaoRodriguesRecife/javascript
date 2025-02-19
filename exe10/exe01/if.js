e5a24dd7122e76d204cb2775283792b7ccf37876
Joao Rodrigues <110858961+JoaoRodriguesRecife@users.noreply.github.com>
2025-02-19 17:26:34 -0300

    atualizando

---
+++ exe10/exe01/if.js
@@ -0,0 +1,8 @@
+var vel = 80.5
+console.log(`vc esta em ${vel} km/h`);
+if (vel > 60){
+    console.log('vc passou da velocidade permitida , MULTADO')
+}else{
+    console.log('use sempre o cinto de segurança')
+
+}
---
+++ exe10/exe02/else.js
@@ -0,0 +1,6 @@
+var t = 'frança'
+if (t == 'brasil'){
+    console.log('vc e brasieliro')
+}else{
+    console.log('voce e instrageiro')
+}
