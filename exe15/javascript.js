52b991cae7605c8479afaa7c4b8564c183c117b2
Joao Rodrigues <110858961+JoaoRodriguesRecife@users.noreply.github.com>
2025-02-23 19:35:12 -0300

    atualizado

--- exe15/javascript.js
+++ exe15/javascript.js
@@ -3,17 +3,23 @@ var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
 //var hora = data.getHours()
-var hora = 8
+var hora = 20
 
 msg.innerHTML = `<p>agora sao ${hora} horas</p>`
 if (hora >= 0 && hora < 12) {
         img.src = 'fotomanha.png'
-            //bom dia 
+        msg.innerHTML += 'bom dia'
+        document.body.style.background = '#cdcf34'
+            
 }else if (hora >= 12 && hora < 18){
         img.src = 'fototarde.png'
-        //boa tarde
+        msg.innerHTML += 'boa tarde' 
+        document.body.style.background = '#cd9621'
+        
 }else {
     img.src = 'fotonoite.png'
+    msg.innerHTML += 'boa noite'
+    document.body.style.background = '#260d3f'
 
 }
 
