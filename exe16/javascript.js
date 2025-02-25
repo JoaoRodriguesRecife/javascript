d3e9964c66bdd2fb0615c38f4f21ce3167257830
Joao Rodrigues <110858961+JoaoRodriguesRecife@users.noreply.github.com>
2025-02-24 21:02:10 -0300

    atualizando

--- exe16/javascript.js
+++ exe16/javascript.js
@@ -1,4 +1,23 @@
-function auto() {
-    
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano ){
+        window.alert('verifique o ano')
+
+    } else {
+        var fsex = document.getElementsByClassName('sexm')
+        var idade = ano - Number(fano.value)
+       var genero = ''
+       if (fsex[0].checked){
+        genero = 'Homem'
+       } else if (fsex[1].checked){
+        genero ='Mulher'
+       }
+       res.style.textAlign = 'center'
+       res.innerHTML = `detectamos  ${genero} com ${idade} anos`
+    }
+        
     
     }
