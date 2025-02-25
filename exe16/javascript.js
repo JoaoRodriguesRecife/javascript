070af69c2b6e12cf932923c05f463deff8f0bed5
Joao Rodrigues <110858961+JoaoRodriguesRecife@users.noreply.github.com>
2025-02-25 12:25:59 -0300

    atualizando

--- exe16/javascript.js
+++ exe16/javascript.js
@@ -1,19 +1,38 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
+    var tano = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fano.value.length == 0 || fano.value > ano ){
+    if (tano.value.length == 0 || tano.value > ano ){
         window.alert('verifique o ano')
 
     } else {
-        var fsex = document.getElementsByClassName('sexm')
-        var idade = ano - Number(fano.value)
+        var fsex = document.getElementsByName('sexm')
+        var idade = ano - Number(tano.value)
        var genero = ''
        if (fsex[0].checked){
         genero = 'Homem'
+        if (idade >= 0 && idade < 10){
+            //criança
+            img.src = '' 
+        } else if(idade < 21){
+            //jovem
+        } else if (idade < 50){
+            //adulto
+        } else {
+            //idoso
+        }
        } else if (fsex[1].checked){
         genero ='Mulher'
+        if (idade >= 0 && idade < 10){
+            //criança
+        } else if(idade < 21){
+            //jovem
+        } else if (idade < 50){
+            //adulto
+        } else {
+            //idoso
+        }
        }
        res.style.textAlign = 'center'
        res.innerHTML = `detectamos  ${genero} com ${idade} anos`
