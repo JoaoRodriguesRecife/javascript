1a7a57b2eb3730bdb777908584c560144d5df5c7
Joao Rodrigues <110858961+JoaoRodriguesRecife@users.noreply.github.com>
2025-03-01 20:57:32 -0300

    atualizando

---
+++ exe17/for.js
@@ -0,0 +1,11 @@
+/*var c = 1
+while(c <=6){
+    console.log(c)
+    c++
+}
+    */
+//-------------
+
+for(var c = 1;c<=10;c++){
+    console.log(`ponto ${c}`)
+}
---
+++ exe18/javascript.js
@@ -0,0 +1,10 @@
+function contar(){
+    var inicio = document.getElementById('inicio1')
+    var fim = document.getElementById('fim1')
+    var passo = document.getElementById('passo1')
+    var res = document.getElementById('res')
+    for(inicio;passo<=fim;c++){
+        
+    }
+    res.addEventListener(res)
+}
