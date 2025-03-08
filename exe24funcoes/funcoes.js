67c3e85cd4a0b1349d1f017fc2a667a78350cfec
Joao Rodrigues <110858961+JoaoRodriguesRecife@users.noreply.github.com>
2025-03-08 16:30:55 -0300

    atualizado

---
+++ exe24funcoes/funcoes.js
@@ -0,0 +1,16 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else{
+        return 'impar'
+    }
+
+    
+}
+/*console.log(parimpar(5)) */
+//outro modelo
+
+
+let res = parimpar(6)
+console.log(res)
+
